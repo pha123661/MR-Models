bf7092fe1a872069148aa0884bbafe53d2652615
Oscar <pha123661@gmail.com>
2024-06-01 18:40:55 +0800

    Update config

--- TC-Eval/serve_tgi.bash
+++ TC-Eval/serve_tgi.bash
@@ -1,7 +1,7 @@
-model=liswei/OpenELM-270M-zh-sft
+# usage: serve_tgi.bash <hf-model-id> <port>
 volume=$PWD/tgi_data # share a volume with the Docker container to avoid downloading weights every run
 token_volume=~/.cache/huggingface/token
 
-docker run --gpus all --shm-size 64g -p 8080:80 -v $volume:/data -v $token_volume:/root/.cache/huggingface/token \
+docker run --gpus all --shm-size 24g -p $2:80 -v $volume:/data -v $token_volume:/root/.cache/huggingface/token \
     ghcr.io/huggingface/text-generation-inference:2.0.4 \
-    --model-id $model --trust-remote-code
+    --model-id $1 --trust-remote-code
