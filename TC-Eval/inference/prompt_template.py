cdb3b05813fc1c6dd3289f00c0956eea79830cff
Oscar <pha123661@gmail.com>
2024-06-03 20:46:16 +0800

    Add TAIDE results

--- TC-Eval/inference/prompt_template.py
+++ TC-Eval/inference/prompt_template.py
@@ -2,7 +2,7 @@ class ModelTemplate:
     @staticmethod
     def apply(query: str, **kwargs):
         return query
-    
+
 
 class Llama2Template(ModelTemplate):
     @staticmethod
@@ -10,10 +10,19 @@ class Llama2Template(ModelTemplate):
         template = "<s> [INST] <<SYS>> {sys_prompt} <</SYS>> {query} [/INST]"
         assert 'sys_prompt' in kwargs, f"sys_prompt text not found; llama2 template = {template}"
         sys_prompt = kwargs['sys_prompt']
-        
+
         out_text = template.format(query=query, sys_prompt=sys_prompt)
         return out_text
-        
+
+class Llama3Template(ModelTemplate):
+    @staticmethod
+    def apply(query: str, **kwargs):
+        template = "<|begin_of_text|><|start_header_id|>system<|end_header_id|>\n\n{sys_prompt}<|eot_id|><|start_header_id|>user<|end_header_id|>\n\n{query}<|eot_id|><|start_header_id|>assistant<|end_header_id|>"
+        assert 'sys_prompt' in kwargs, f"sys_prompt text not found; llama3 template = {template}"
+        sys_prompt = kwargs['sys_prompt']
+
+        out_text = template.format(query=query, sys_prompt=sys_prompt)
+        return out_text
 
 class VicunaTemplate(ModelTemplate):
     @staticmethod
@@ -22,7 +31,7 @@ class VicunaTemplate(ModelTemplate):
         template = "{sys_prompt} \nUSER: {query}\nASSISTANT: "
         assert 'sys_prompt' in kwargs, f"sys_prompt text not found; vicuna template = {template}"
         sys_prompt = kwargs['sys_prompt']
-        
+
         out_text = template.format(query=query, sys_prompt=sys_prompt)
         return out_text
 
@@ -30,6 +39,7 @@ class VicunaTemplate(ModelTemplate):
 ALL_MODEL_TEMPLATE_FUNC={
     'default': ModelTemplate.apply,
     'llama2': Llama2Template.apply,
+    'llama3': Llama3Template.apply,
     'vicuna': VicunaTemplate.apply,
     'openai': ModelTemplate.apply
 }
