50d831cccc685cbe0e10763cd8ef2cb5bcf4ba40
Oscar <pha123661@gmail.com>
2024-06-04 14:06:58 +0800

    Update leaderboard

--- TC-Eval/reshape_md.py
+++ TC-Eval/reshape_md.py
@@ -8,7 +8,7 @@ df = df.drop(columns=[''])
 # append column "metric" after "task_name" with an underscore
 df['task_name'] = df['task_name'].str.strip()
 df['metric'] = df['metric'].str.strip()
-df['task_name'] = df[['task_name', 'metric']].apply(lambda x: x.iloc[0] + '_' + x.iloc[1], axis=1)
+# df['task_name'] = df[['task_name', 'metric']].apply(lambda x: x.iloc[0] + '_' + x.iloc[1], axis=1)
 df = df.drop('metric', axis=1)
 df = df.dropna(axis=1, how='all')
 df = df.iloc[1:]
@@ -19,12 +19,19 @@ df = df.reset_index()
 df = df.rename(columns={'index': 'model'})
 df.columns.name = None
 
+
 # try to convert columns to float
 for col in df.columns:
     if col == 'model':
         continue
-    df[col] = df[col].astype(float).apply(lambda x: f'{100*x:.2f}%')
+    df[col] = df[col].astype(float)
 
+df['average'] = df.iloc[:, 1:].mean(axis=1)
+
+for col in df.columns:
+    if col == 'model':
+        continue
+    df[col] = df[col].map(lambda x: f"{100 * x:.2f}%")
 
 with open('benchmark_T.md', 'w') as f:
     f.write(df.to_markdown(index=False))
